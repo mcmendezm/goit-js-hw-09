6d1603c2cefdc37e1676059b8a48af9476fc2fde
mcmendezm <107286644+mcmendezm@users.noreply.github.com>
2023-06-06 05:16:46 +0000

    Deploying to gh-pages from @ mcmendezm/goit-js-hw-09@4f58f4ecd9d911f4becbbcbadde91a141963d35e 🚀

---
+++ 01-color-switcher.8f4b84b9.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function n(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(n,1e3)})),a.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.8f4b84b9.js.map
